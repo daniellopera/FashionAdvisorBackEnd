f8299f788540b5f0e58af650647c4dbc16f7884d
nmunera@eafit.edu.co <nmunera@eafit.edu.co>
2015-03-22 10:18:41 -0500

    Added a service that updates all the brands in the database.

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -8,8 +8,7 @@ class BrandsController < ApplicationController
   # Updates the fashion advisor database brands with the Shopstyle database new brands.
   def update_brands
     brands_raw = search_brands_shopstyle
-    brands = format_brands_hash(brands_raw)
-
+    brands = format_brands_hash(brands_raw["brands"])
     render json: {status: 0, data: {brands: brands}}
   end
 
