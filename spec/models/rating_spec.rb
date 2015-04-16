94a4692428ad84d86f8f6972e44510cb2bdf146b
Daniel Klinkert <daniel.klinkert@gmail.com>
2015-04-16 15:45:55 -0500

    Add TDD support for rating and user

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
 
-  it " did't save when user don't  add outfit_id" do
+  it "did't save when user don't  add outfit_id" do
 
     expect {Rating.create!(rating:5)}.to  raise_error
+
+  end
+
+  it "saves when user enter rating and the outfit_id" do
+
+    rating = Rating.create!(outfit_id:1,rating:5)
+
+    expect(rating.rating).to eq(5)
+
+  end
+
+  it "did't save when user don't  add a rating" do
+
+    expect {Rating.create!(outfit_id:1)}.to  raise_error
 
   end
 
