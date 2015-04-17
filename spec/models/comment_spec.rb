4bc966333e89cb77940d8f10b6b8afc86490919d
nmunera <nmunera@eafit.edu.co>
2015-04-17 11:42:25 -0500

    Started making the tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
 
-  it "saves when user add outfit_id and comment" do
-    lindeman = Comment.create!(outfit_id: 1, comment: "Lindeman")
+  it "saves a comment in the database" do
+    comment = Comment.create!(outfit_id: 1, comment: "Lindeman")
 
-    expect(lindeman.comment).to eq("Lindeman")
+    expect(Comment.all.count).to eq(1)
 
   end
 
-  it "is invalid without comment" do
+  it "saves a comment with the specific outfits id" do
 
-    #comment = Comment.create!(outfit_id:1)
-    expect {Comment.create!(outfit_id:1)}.to  raise_error
+
   end
 
   it "is invalid with empty comment" do
