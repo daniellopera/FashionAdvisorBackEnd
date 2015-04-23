bb522dbe53ea8e3899368c7ae37bf7d0602169f9
Daniel Lopera <dlopera3@eafit.edu.co>
2015-04-23 15:47:06 -0500

    Refactored tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,13 +6,9 @@ class TagsController < ApplicationController
     unless params[:tags] ==  nil
       tags = params[:tags]
       outfit_id = params[:outfit_id]
-      tag = Tag.new
-      status = tag.create_tags(tags,outfit_id)
+      status = Tag.create_tags(tags,outfit_id)
       render json: {status: status, data: nil}
     end
   end
-
-  def search_tags
-
-  end
+  
 end
